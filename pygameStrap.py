ef410561e4041409e9be9840bc2341941a8803c4
Mayank <33160019+thegitone23@users.noreply.github.com>
2018-10-06 17:01:05 +0530

    Update pygameStrap.py

--- pygameStrap.py
+++ pygameStrap.py
@@ -7,10 +7,30 @@ from pygame.locals import *
 #opengl imports
 from OpenGL.GL import *
 from OpenGL.GLU import *
+#other imports
+import math
 
+#defing functions for basic shapes
+def drawPixel(x,y):
+    glBegin(GL_POINTS)
+    glVertex2f(x,y)
+    glEnd()
+
+def drawLine(a,b,c,d):
+    glBegin(GL_LINES)
+    glVertex2f(a,b)
+    glVertex2f(c,d)
+    glEnd()
+
+def drawCircle(x,y,r=50):
+    toRad = (math.pi/180)
+    for i in range (0,360):
+        drawPixel(x+(r*math.cos(toRad*i)),y+(r*math.sin(toRad*i)))
+
+#main drawing logic
 def draw():
     #coding logic here
-    print("Working!!!")
+    drawCircle(0,0)
 
 
 #main function
@@ -19,9 +39,8 @@ def main():
     pygame.init()
     display = (800,600) #the pygame windows resolution
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    gluPerspective(65,display[0]/display[1],0.1,20.0)
-    glTranslatef(0.0,0.0,-5)
-    
+    gluOrtho2D(0,800,600,0) #defining drawing limits
+
     #the main loop 
     while True:
 
@@ -30,7 +49,7 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        
+
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) #clear the frame
         draw() #calling the function with drawing logic
         pygame.display.flip() #bring up the updated screen
